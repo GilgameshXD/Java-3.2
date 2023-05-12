d850077e0b1ad594a9b256d29e3cc82742d3ecf8
Alexander <brolyspowerismaximum@gmail.com>
2023-05-12 21:25:26 +0500

    Исправил имена переменных, согласно камелкейсу.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int currentbalance = 100;                   // Объявляете переменные для входных данных и
-        int topupamount = 1100;                      // параметров программы: начального счёта,
-        int minimumlimitforbonus = 1000;            // суммы пополнения и тп
-        int newbalance;
+        int currentBalance = 100;                   // Объявляете переменные для входных данных и
+        int topUpAmount = 1100;                      // параметров программы: начального счёта,
+        int minimumLimitForBonus = 1000;            // суммы пополнения и тп
+        int newBalance;
         int bonuses = 0;
 
-        if (topupamount > minimumlimitforbonus) {                                                            // Условным оператором проверяете, превысила ли
-            newbalance = currentbalance + topupamount + topupamount / 100;                                   // сумма пополнения порог, и для этих двух разных
-            bonuses = topupamount / 100;                                                                     // сценариев рассчитываете сумму бонуса и выводите
+        if (topUpAmount > minimumLimitForBonus) {                                                            // Условным оператором проверяете, превысила ли
+            newBalance = currentBalance + topUpAmount + topUpAmount / 100;                                   // сумма пополнения порог, и для этих двух разных
+            bonuses = topUpAmount / 100;                                                                     // сценариев рассчитываете сумму бонуса и выводите
         } else {                                                                                             // на экран.
-            newbalance = currentbalance + topupamount;
+            newBalance = currentBalance + topUpAmount;
         }
-        System.out.println("Ваш баланс: " + newbalance + " рубль(-ей)" + "\nБонусов зачислено: " + bonuses);
+        System.out.println("Ваш баланс: " + newBalance + " рубль(-ей)" + "\nБонусов зачислено: " + bonuses);
     }
 }
